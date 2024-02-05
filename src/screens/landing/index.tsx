1a20a44d83f8114757eec7e7f6b2906e36302a38
Cristóbal <cristobalsepulveda1@hotmail.com>
2024-02-05 19:44:06 -0300

    feat: first screen and button component.

---
+++ src/screens/landing/index.tsx
@@ -0,0 +1,27 @@
+import Button from "../../components/button";
+import logo from "../../assets/Main.png";
+import "./landing.css";
+
+function Landing() {
+  return (
+    <div id="landing">
+      <div id="image_container">
+        <img src={logo} alt="counter logo" />
+      </div>
+
+      <div id="text_container">
+        <h1>Welcome to Counters</h1>
+        <p>
+          Capture cups of lattes, frapuccinos or anything else that can be
+          counted.
+        </p>
+      </div>
+
+      <div id="button_container">
+        <Button text="Get started" />
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
