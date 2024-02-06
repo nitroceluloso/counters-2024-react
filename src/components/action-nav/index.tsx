1636d8b3bffb886d84451ddc02a058c356286a63
Cristóbal <cristobalsepulveda1@hotmail.com>
2024-02-06 19:05:05 -0300

    feat: add action-nav component.

--- src/components/action-nav/index.tsx
+++ src/components/action-nav/index.tsx
@@ -1,5 +1,24 @@
-function ActionNav() {
-    return (<></>)
+import Button from "../button";
+import "./action-nav.css";
+
+type ActionNavProps = {
+  showOptional?: boolean;
+};
+
+function ActionNav({ showOptional = false }: ActionNavProps) {
+  return (
+    <div id="action-nav">
+      <div>
+        <Button icon="add" />
+      </div>
+      {showOptional && (
+        <div id="optional">
+          <Button icon="delete" />
+          <Button icon="share" />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ActionNav;
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,18 +1,24 @@
 import { className } from "../../utils/classname";
 import "./button.css";
+import { getIcon } from "./button.helper";
 
-type IconVariant = '';
+export type IconVariant = "add" | "delete" | "share";
 
-type ButtonProps = {
+interface ButtonProps {
   text?: string;
   variant?: "main" | "secundary";
   icon?: IconVariant;
   onClick?: () => void;
-};
+}
 
-function Button({ text, variant = "main" }: ButtonProps) {
+function Button({ text = "", variant = "main", icon, onClick }: ButtonProps) {
   const classString = className("button", variant);
-  return <button className={classString}>{text}</button>;
+  const content = text ? text : <img src={getIcon(icon!)} alt="" />;
+  return (
+    <button className={classString} onClick={onClick}>
+      {content}
+    </button>
+  );
 }
 
 export default Button;
